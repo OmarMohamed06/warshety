168ecee230cfc5a09bbed36e2141499fb7823dbf
Omar Mohamed <Dell@MacBook-Pro.local>
2026-08-16 17:46:52 +0300

    Remove download app section from footer; replace car icon with real logo in phone mockup

--- src/components/app-download/PhoneMockup.tsx
+++ src/components/app-download/PhoneMockup.tsx
@@ -44,12 +44,8 @@ export function PhoneMockup({ className }: { className?: string }) {
 
           {/* App header */}
           <div className="px-4 py-2 flex items-center gap-2">
-            <div className="w-6 h-6 rounded-lg bg-[#FF4B19] flex items-center justify-center">
-              <svg viewBox="0 0 24 24" className="w-3.5 h-3.5 fill-white">
-                <path d="M18.92 6.01C18.72 5.42 18.16 5 17.5 5h-11c-.66 0-1.21.42-1.42 1.01L3 12v8c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-1h12v1c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-8l-2.08-5.99zM6.5 16c-.83 0-1.5-.67-1.5-1.5S5.67 13 6.5 13s1.5.67 1.5 1.5S7.33 16 6.5 16zm11 0c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zM5 11l1.5-4.5h11L19 11H5z" />
-              </svg>
-            </div>
-            <span className="text-white text-[10px] font-black">Warshety</span>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img src="/warshety-nav.svg" alt="Warshety" className="h-6 w-auto object-contain" />
           </div>
 
           {/* Booking card */}
